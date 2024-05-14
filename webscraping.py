9204819f4f130de656476f067da242bb40cf96f4
Jai Panchal <jaipanchal97@gmail.com>
2024-05-14 23:47:20 +0530

    done

---
+++ webscraping.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+
+web_text=requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
+
+
+soup=bs4.BeautifulSoup(web_text.text,'lxml')
+computer=soup.select('.mw-file-description')[0]
+
+image_link=requests.get('https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Deep_Blue.jpg/220px-Deep_Blue.jpg')
+with open('my_com_image.jpg','wb') as f:
+    f.write(image_link.content)
