6e654dd2ce5913ee33898ead9d1ccaccc5763f48
Jai Panchal <jaipanchal97@gmail.com>
2024-05-15 15:16:03 +0530

    sweb scraping using beautiful soup

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 import bs4
 
 web_text=requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
@@ -10,3 +10,7 @@ computer=soup.select('.mw-file-description')[0]
 image_link=requests.get('https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Deep_Blue.jpg/220px-Deep_Blue.jpg')
 with open('my_com_image.jpg','wb') as f:
     f.write(image_link.content)
+
+
+'''
+from . import web_scraping2
